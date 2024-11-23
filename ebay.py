c5ec168c44abc4567fd21d28d34099c95406b736
Muhammad Waseem <mw11221122@gmail.com>
2024-11-23 09:10:07 -0800

    working on get_data function

--- ebay.py
+++ ebay.py
@@ -1,4 +1,10 @@
-import requests, time, re
-import pandas as pd
+import requests, re, time
 from bs4 import BeautifulSoup
+import pandas as pd
 
+def get_data():
+    res = requests.get('https://www.google.com/')
+    print(res.status_code)
+
+
+get_data()
