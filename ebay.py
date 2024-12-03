72d4a59f285dd1b0f4ff0d126fc62e59272a7b0b
Muhammad Waseem <mw11221122@gmail.com>
2024-12-03 05:57:17 -0800

    all good

--- ebay.py
+++ ebay.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 def get_data():
-    res = requests.get('https://www.ebay.com/')
+    res = requests.get('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1313&_nkw=camera&_sacat=0')
     soup = BeautifulSoup(res.content, 'html.parser')
     return soup
 
 
 def parse(soup):
-    print(soup.title.text)
+    product_links = soup.find_all('div', class_ = 's-item__info clearfix')
+    print(product_links[0])
     
 parse(get_data())
